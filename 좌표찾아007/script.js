4ca847c6a147d71ddfe0ada17d64588ae6359ed8
hyunjeongPark <GuoHJP@gmail.com>
2021-02-03 03:31:42 +0900

    png사이즈 변경

--- 좌표찾아007/script.js
+++ 좌표찾아007/script.js
@@ -4,11 +4,11 @@ let lineY = document.querySelector(".lineY");
 
 window.addEventListener("mousemove", (e)=>{
     coor.innerHTML = `${e.pageX}, ${e.pageY}`;
-    coor.style.left = `${e.pageX+90}px`;
-    coor.style.top = `${e.pageY+75}px`;
+    coor.style.left = `${e.pageX+25}px`;
+    coor.style.top = `${e.pageY+50}px`;
     lineX.style.left = "0px";
-    lineX.style.top = `${e.pageY+26.5}px`;
+    lineX.style.top = `${e.pageY}px`;
     lineY.style.top = "0px";
-    lineY.style.left = `${e.pageX+59}px`;
+    lineY.style.left = `${e.pageX}px`;
 })
 
