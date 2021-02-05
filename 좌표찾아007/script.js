a33613d6a50c7b13cf571a699da96cc336349677
hyunjeongPark <GuoHJP@gmail.com>
2021-02-05 10:38:12 +0900

    성능 개선(top, left --> translate)

--- 좌표찾아007/script.js
+++ 좌표찾아007/script.js
@@ -2,16 +2,25 @@ const coor = document.querySelector(".coor");
 const lineX = document.querySelector(".lineX");
 const lineY = document.querySelector(".lineY");
 const target = document.querySelector(".target");
+const targetRect = target.getBoundingClientRect();
+const targetRectWidth = targetRect.width / 2;
+const targetRectHeight = targetRect.height / 2;
 
 window.addEventListener("mousemove", (e)=>{
     const x = e.pageX;
     const y = e.pageY;
+    // coor.innerHTML = `${x}px, ${y}px`;
+    // target.style.left= `${x}px`
+    // target.style.top= `${y}px`
+    // coor.style.left = `${x}px`;
+    // coor.style.top = `${y}px`;
+    // lineX.style.top = `${y}px`;
+    // lineY.style.left = `${x}px`;
+
     coor.innerHTML = `${x}px, ${y}px`;
-    target.style.left= `${x}px`
-    target.style.top= `${y}px`
-    coor.style.left = `${x}px`;
-    coor.style.top = `${y}px`;
-    lineX.style.top = `${y}px`;
-    lineY.style.left = `${x}px`;
+    target.style.transform = `translate(${x - targetRectWidth}px, ${y - targetRectHeight}px)`;
+    coor.style.transform = `translate(${x}px, ${y}px)`;
+    lineX.style.transform = `translateY(${y}px)`;
+    lineY.style.transform = `translateX(${x}px)`;
 })
 
