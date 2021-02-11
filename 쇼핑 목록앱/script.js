e5c1a740f3bfb4f67ccf9ffe2818b2d409b9b121
hyunjeongPark <GuoHJP@gmail.com>
2021-02-12 07:58:44 +0900

    edit 추가

--- 쇼핑 목록앱/script.js
+++ 쇼핑 목록앱/script.js
@@ -13,9 +13,9 @@ function addListHandler(id){
     input.classList.remove("change");
     input.placeholder = placeholder;
     const listText = `
-    <li id=${id}><span class = "inputText">${inputValue}</span><i class="fas fa-trash-alt"></i></li>
+    <li id=${id}><span class = "inputText">${inputValue}</span><i class="far fa-edit"></i><i class="fas fa-trash-alt"></i></li>
     `;
-    LIST.push({list: inputValue, id: id});
+    LIST.push({text: inputValue, id: id});
     saveLocalStorage();
     ul.insertAdjacentHTML("beforeend", listText);
     input.value = ""
@@ -31,13 +31,44 @@ function deleteHandler(e){
     saveLocalStorage();
 }
 
+function editHandler(e){
+    const li = e.target.parentNode;
+    li.childNodes[1].style.display = "none";
+    li.childNodes[2].style.display = "none";
+    const id = li.id;
+    let list = li.childNodes[0];
+    let listText = li.childNodes[0].innerText;
+    const editInput = document.createElement("input");
+    editInput.classList.add("editInput");
+    editInput.value = listText;
+    li.replaceChild(editInput, list);
+    editInput.addEventListener("keydown", (e)=>{
+        if(e.key !== "Enter"){
+            return;
+        }else{
+            // if(typeof editInput.value == "undefined" || editInput.value == null || editInput.value == ""){
+            //     editInput.classList.add("change");
+            //     editInput.placeholder = "수정할 목록을 입력해주세요";
+            //     return;
+            // }
+            li.childNodes[1].style.display = "";
+            li.childNodes[2].style.display = "";
+            const editValue = editInput.value;
+            list.innerText = editValue;
+            li.replaceChild(list, editInput);
+            LIST[id].text = editValue;
+            saveLocalStorage();
+        }
+    })
+}
+
 
 
 // 로컽스토리지
 function loadToDo(LIST){
     LIST.forEach((item)=>{
         const listText = `
-        <li id=${item.id}><span class = "inputText">${item.list}</span><i class="fas fa-trash-alt"></i></li>
+        <li id=${item.id}><span class = "inputText">${item.text}</span><i class="far fa-edit"></i><i class="fas fa-trash-alt"></i></li>
         `;
         ul.insertAdjacentHTML("beforeend", listText);
     })
@@ -64,9 +95,11 @@ document.addEventListener("click", (e)=>{
     e.preventDefault();
     if(e.target.classList.contains("fa-trash-alt")){
         deleteHandler(e);
-    } else if(e.target.classList.contains("fa-plus-circle")){
+    }else if(e.target.classList.contains("fa-plus-circle")){
         addListHandler(id);
         id++
+    }else if(e.target.classList.contains("fa-edit")){
+        editHandler(e);
     }
 })
 
